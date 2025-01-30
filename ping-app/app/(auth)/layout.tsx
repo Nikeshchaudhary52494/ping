d6a76ccf0c8049dd82480760369860ba9bf8e9b4
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2025-01-30 17:54:11 +0530

    auth redirect corrected

--- ping-app/app/(auth)/layout.tsx
+++ ping-app/app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
+import { getUser } from "@/actions/user/getUser";
 import type { Metadata } from "next";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 
 export const metadata: Metadata = {
@@ -7,11 +9,15 @@ export const metadata: Metadata = {
     description: "Ping authentication",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const { user } = await getUser();
+    if (user) redirect("/")
+
     return (
         <div className="flex bg-[#020817] h-full">
             <div className="relative w-1/3">
